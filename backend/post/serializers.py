a51bfabb5b0a376bba054d46476e0e20dc46065f
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2021-10-11 17:17:01 +0200

    final

---
+++ backend/post/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        # 1 comment might be missing or to be ajusted for the fields:
+        #fields = ['id', 'author', 'title', 'content', 'images', 'likes', 'comment', 'created', 'updated']
+        fields = ['id', 'author', 'title', 'content', 'images', 'likes', 'created', 'updated']
+        read_only_fields = ['author']
