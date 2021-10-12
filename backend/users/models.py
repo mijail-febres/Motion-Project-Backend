635372d3e014a0eebbc920ab43bcebc0ad018ff4
David Furrer <david.furrer1@gmail.com>
2021-10-12 09:52:43 +0200

    small fixes

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
-from backend.post.models import Post
+from post.models import Post
 
 def user_directory_path(instance, filename):
     return f'{instance.id}/{filename}'
