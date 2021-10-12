1e10f623bdefc9663daf21a8ef342b72989d3a7a
mijaplex <mijail.febres@gmail.com>
2021-10-12 09:09:35 +0200

    to merge

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python -c "import time; time.sleep(3)"
+python -c "import time;  time.sleep(3)"
 python manage.py collectstatic --no-input
 python manage.py migrate
 gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
