13dcc4eef40dc764f5ec5af818b748819a1c534b
David Furrer <david.furrer1@gmail.com>
2021-10-12 09:20:20 +0200

    adding o user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
+def user_directory_path(instance, filename):
+    return f'{instance.id}/{filename}'
+
 # Create your models here.
 class User(AbstractUser):
     # Field used for authentication
@@ -11,6 +15,11 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['username']
 
     email = models.EmailField(unique=True)
+
+    avatar = models.ImageField(upload_to=user_directory_path, blank=True, null=True)
+
+
+
 
     def __str__(self):
         return self.email
