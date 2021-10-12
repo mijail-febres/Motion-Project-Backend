daec11b1ba060b6f34316e665af309abb8f9d37c
David Furrer <david.furrer1@gmail.com>
2021-10-12 16:30:14 +0200

    cleaning

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,8 +2,6 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from users.models import User
 
-# User = get_user_model()
-
 
 class UserSerializer(serializers.ModelSerializer):
    
