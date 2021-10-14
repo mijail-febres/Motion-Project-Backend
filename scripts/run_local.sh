43f3185d00c0d60a554ab59e7effc91086eb6256
David Furrer <david.furrer1@gmail.com>
2021-10-14 09:03:47 +0200

    fixing urls for posts

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python -c "import time;  time.sleep(3)"
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0:8000
