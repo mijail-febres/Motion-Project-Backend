a6aebb71c8c9964baa36d8e3ea2c9f0048f20c4a
David Furrer <david.furrer1@gmail.com>
2021-10-14 09:27:18 +0200

    adding Mijail's changes

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -24,3 +24,15 @@ class UserFollowSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "email", "followers", "followees"]
+
+
+class UserFriendsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "friend_request", "requested_from", "friends_with"]
+
+
+class UserRequestFriendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username"]
